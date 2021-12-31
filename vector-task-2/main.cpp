e4c5c69641e172d40b4ea87bf042849698e22d13
Maciej Białczak <m.bialczak@protonmail.com>
2021-12-31 17:06:33 +0100

    vector task 2 completed

---
+++ vector-task-2/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+void printSizeCapacity(const std::vector<int>& vec)
+{
+    std::cout << "vec size: " << vec.size()
+              << "\nvec capacity: " << vec.capacity()
+              << std::endl;
+}
+
+int main(int argc, char const* argv[])
+{
+    std::vector<int> vec;
+    printSizeCapacity(vec);
+
+    vec.resize(10, 5);
+    printSizeCapacity(vec);
+
+    vec.reserve(20);
+    printSizeCapacity(vec);
+
+    vec.shrink_to_fit();
+    printSizeCapacity(vec);
+
+    return 0;
+}
