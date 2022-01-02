850a06a9584e395068c0e7d61e5e533829c74181
Maciej Białczak <m.bialczak@protonmail.com>
2022-01-02 21:15:37 +0100

    forward-list-task completed

---
+++ forward-list-task/main.cpp
@@ -0,0 +1,36 @@
+#include <array>
+#include <forward_list>
+#include <iostream>
+
+// prints all container elements to console
+template <class Container>
+void print(const Container& container)
+{
+    for (auto&& el : container) {
+        std::cout << el << ' ';
+    }
+    std::cout << std::endl;
+}
+
+int main(int argc, char const* argv[])
+{
+    std::forward_list elements { 0, 1, 2, 3, 4, 5, 6 };
+
+    print(elements);
+
+    auto it = elements.begin();
+    std::advance(it, 1);
+    elements.erase_after(it);
+    elements.emplace_front(10);
+    elements.reverse();
+    elements.emplace_front(10);
+    elements.reverse();
+    print(elements);
+
+    it = elements.begin();
+    std::advance(it, 2);
+    elements.emplace_after(it, 20);
+
+    print(elements);
+    return 0;
+}
