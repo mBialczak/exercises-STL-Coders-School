303c242d46333a556a01911b89dbf643e48ac05a
Maciej Białczak <m.bialczak@protonmail.com>
2022-01-13 11:33:32 +0100

    Algorithms task 1 with UT completed

---
+++ algorithms-task-1/main.cpp
@@ -0,0 +1,14 @@
+// Utwórz std::vector<int> v = {8, 2, 5, 3, 4, 4, 2, 7, 6, 6, 1, 8, 9};
+// Policz, ile jest elementów większych lub równych 5
+// Sprawdź, czy istnieje element mniejszy od 1
+// Sprawdź, czy wszystkie elementy są większe od 1
+
+#include <algorithm>
+#include <vector>
+
+int main(int argc, char const* argv[])
+{
+    std::vector<int> v = { 8, 2, 5, 3, 4, 4, 2, 7, 6, 6, 1, 8, 9 };
+
+    return 0;
+}
---
+++ algorithms-task-1/test.cpp
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <gtest/gtest.h>
+#include <vector>
+
+// Utwórz std::vector<int> v = {8, 2, 5, 3, 4, 4, 2, 7, 6, 6, 1, 8, 9};
+
+// Policz, ile jest elementów większych lub równych 5
+TEST(testElementsCount, testElementsLessOrEqualFive)
+{
+    std::vector<int> v = { 8, 2, 5, 3, 4, 4, 2, 7, 6, 6, 1, 8, 9 };
+    auto less_or_equal_five_count = std::count_if(v.begin(),
+                                                  v.end(),
+                                                  [](const auto el) {
+                                                      return el <= 5;
+                                                  });
+    ASSERT_EQ(7, less_or_equal_five_count);
+}
+
+// Sprawdź, czy istnieje element mniejszy od 1
+TEST(testElementsCount, testIfAnyLessThanOne)
+{
+    std::vector<int> v = { 8, 2, 5, 3, 4, 4, 2, 7, 6, 6, 1, 8, 9 };
+    bool any_less_than_five = std::any_of(begin(v), end(v), [](const auto el) {
+        return el < 1;
+    });
+    ASSERT_EQ(false, any_less_than_five);
+}
+
+// Sprawdź, czy wszystkie elementy są większe od1
+TEST(testElementsCount, testIfAnyGreaterThanOne)
+{
+    std::vector<int> v = { 8, 2, 5, 3, 4, 4, 2, 7, 6, 6, 1, 8, 9 };
+    bool any_greater_than_five = std::any_of(begin(v), end(v), [](const auto el) {
+        return el > 1;
+    });
+    ASSERT_EQ(true, any_greater_than_five);
+}
+
+int main(int argc, char** argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+    return 0;
+}
