230c751ee73278b32ffa5bb98c08ae0a784ce14e
Maciej Białczak <m.bialczak@protonmail.com>
2022-01-29 21:15:29 +0100

    Part A implemented

--- map-0x05/main.cpp
+++ map-0x05/main.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+#include <cmath>
 #include <iostream>
 #include <map>
 #include <string>
@@ -32,5 +34,36 @@ struct Point
 
 int main(int argc, char const* argv[])
 {
+    // 1a.
+    std::map<std::string, Point> citiesA = {
+        { "Wrocław", { 17, 51 } },
+        { "Moskwa", { 47, 55 } },
+        { "Nowy Jork", { -74, 40 } },
+        { "Sydney", { 151, -33 } }
+    };
+
+    // 2a. Sprawdź czy w mapie jest element, który znajduje się w promieniu 70
+    //      od środka układu współrzędnych (0, 0)
+
+    bool is_city_in_radious =
+        std::ranges::any_of(citiesA.cbegin(),
+                            citiesA.cend(),
+                            [](const auto& entry) {
+                                auto [x, y] = entry.second;
+                                auto distance_squared = pow(x, 2) + pow(y, 2);
+                                constexpr auto radious_sqared = pow(70, 2);
+                                return distance_squared < radious_sqared;
+                            });
+
+    std::cout << "There is a city in range below 70: "
+              << std::boolalpha << is_city_in_radious << '\n';
+
+    // 3a. Pobierz i wypisz współrzędne Sydney
+    auto search_result = citiesA.find("Sydney");
+    if (search_result != citiesA.end()) {
+        std::cout << "Sydney coords: x="
+                  << search_result->second.x
+                  << " y=" << search_result->second.y << std::endl;
+    }
     return 0;
 }
