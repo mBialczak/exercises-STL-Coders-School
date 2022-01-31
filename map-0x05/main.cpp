38e1a5667738ef776287431bf251b4ae4ac9608c
Maciej Białczak <m.bialczak@protonmail.com>
2022-01-31 22:08:36 +0100

    Subtask 1a and 2a implemented

--- map-0x05/main.cpp
+++ map-0x05/main.cpp
@@ -1,8 +1,8 @@
 #include <algorithm>
 #include <cmath>
 #include <iostream>
-#include <map>
 #include <string>
+#include <unordered_map>
 /*
 Struktura Point ma reprezentować punkt o współrzędnych x i y na mapie. Mamy też dane 4 miast:
     Wrocław (x = 17, y = 51)
@@ -12,14 +12,14 @@ Struktura Point ma reprezentować punkt o współrzędnych x i y na mapie. Mamy 
 
 Część A - łatwiejsza
 
-  1a. Stwórz std::map<std::string, Point>, która będzie przechowywać powyższe miejsca
+  1a. Stwórz std::unordered_map<std::string, Point>, która będzie przechowywać powyższe miejsca
   2a. Sprawdź czy w mapie jest element, który znajduje się w promieniu 70
       od środka układu współrzędnych (0, 0)
   3a. Pobierz i wypisz współrzędne Sydney
 
 Część B - trudniejsza
 
-  1b. Skopiuj te dane do mapy std::map<Point, std::string>
+  1b. Skopiuj te dane do mapy std::unordered_map<Point, std::string>
   2b. Sprawdź czy w odwróconej mapie jest element, który znajduje się w promieniu 70
       od środka układu współrzędnych (0, 0)
   3b. Pobierz i wypisz współrzędne Sydney
@@ -40,10 +40,10 @@ bool operator<(const Point lhs, const Point rhs)
     return lhs_distance_sq < rhs_distance_sq;
 }
 
-int main(int argc, char const* argv[])
+int main()
 {
     // 1a.
-    std::map<std::string, Point> citiesA = {
+    std::unordered_map<std::string, Point> citiesA = {
         { "Wrocław", { 17, 51 } },
         { "Moskwa", { 47, 55 } },
         { "Nowy Jork", { -74, 40 } },
@@ -55,56 +55,55 @@ int main(int argc, char const* argv[])
     auto point_in_range = [radius = 70](auto point) {
         auto [x, y] = point;
         auto distance_squared = pow(x, 2) + pow(y, 2);
-        return distance_squared < pow(radius, 2);
+        return std::pair<bool, double> { distance_squared < pow(radius, 2),
+                                         distance_squared };
     };
 
-    bool is_city_in_radious = std::ranges::any_of(citiesA.cbegin(),
-                                                  citiesA.cend(),
-                                                  [&](const auto& entry) {
-                                                      return point_in_range(entry.second);
-                                                  });
-
-    std::cout << "There is a city in range below 70: "
-              << std::boolalpha << is_city_in_radious << '\n';
-
-    // 3a. Pobierz i wypisz współrzędne Sydney
-    auto search_result = citiesA.find("Sydney");
-    if (search_result != citiesA.end()) {
-        std::cout << "Sydney coords: x="
-                  << search_result->second.x
-                  << " y=" << search_result->second.y << std::endl;
+    for (const auto& [city, point] : citiesA) {
+        auto [is_in_range, range] = point_in_range(point);
+        if (is_in_range) {
+            std::cout << city << "-> distance from origin: " << range << std::endl;
+        }
     }
 
-    //  1b. Skopiuj te dane do mapy std::map<Point, std::string>
-    std::map<Point, std::string> citiesB;
-    std::transform(citiesA.begin(),
-                   citiesA.end(),
-                   std::inserter(citiesB, citiesB.begin()),
-                   [](const auto& el) -> std::pair<Point, std::string> {
-                       return { el.second, el.first };
-                   });
+    // // 3a. Pobierz i wypisz współrzędne Sydney
+    // auto search_result = citiesA.find("Sydney");
+    // if (search_result != citiesA.end()) {
+    //     std::cout << "Sydney coords: x="
+    //               << search_result->second.x
+    //               << " y=" << search_result->second.y << std::endl;
+    // }
 
-    // 2b. Sprawdź czy w odwróconej mapie jest element, który znajduje się w promieniu 70
-    // od środka układu współrzędnych (0, 0)
-    auto it = std::ranges::find_if(citiesB,
-                                   [&](const auto& entry) {
-                                       return point_in_range(entry.first);
-                                   });
-    if (it != citiesB.end()) {
-        std::cout << "There is a city in citiesB in range less then 70 from 0,0\n";
-    }
+    // //  1b. Skopiuj te dane do mapy std::map<Point, std::string>
+    // std::map<Point, std::string> citiesB;
+    // std::transform(citiesA.begin(),
+    //                citiesA.end(),
+    //                std::inserter(citiesB, citiesB.begin()),
+    //                [](const auto& el) -> std::pair<Point, std::string> {
+    //                    return { el.second, el.first };
+    //                });
 
-    //  3b. Pobierz i wypisz współrzędne Sydney
-    it = std::ranges::find_if(citiesB,
-                              [&](const auto& entry) {
-                                  return entry.second == "Sydney";
-                              });
+    // // 2b. Sprawdź czy w odwróconej mapie jest element, który znajduje się w promieniu 70
+    // // od środka układu współrzędnych (0, 0)
+    // auto it = std::ranges::find_if(citiesB,
+    //                                [&](const auto& entry) {
+    //                                    return point_in_range(entry.first);
+    //                                });
+    // if (it != citiesB.end()) {
+    //     std::cout << "There is a city in citiesB in range less then 70 from 0,0\n";
+    // }
 
-    if (it != citiesB.end()) {
-        std::cout << "Sydney coords: x="
-                  << search_result->second.x
-                  << " y=" << search_result->second.y << std::endl;
-    }
+    // //  3b. Pobierz i wypisz współrzędne Sydney
+    // it = std::ranges::find_if(citiesB,
+    //                           [&](const auto& entry) {
+    //                               return entry.second == "Sydney";
+    //                           });
+
+    // if (it != citiesB.end()) {
+    //     std::cout << "Sydney coords: x="
+    //               << search_result->second.x
+    //               << " y=" << search_result->second.y << std::endl;
+    // }
 
     return 0;
 }
