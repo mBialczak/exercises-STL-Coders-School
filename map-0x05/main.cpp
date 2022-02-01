6d19375ad7f59a25344b9a98ffa1840f9d89f5a4
Maciej Białczak <m.bialczak@protonmail.com>
2022-02-01 08:25:02 +0100

    Added another version for part B
    - implemented injected hash specialization
    - added another instance of unordered_map for injected hash
    - made some cleanup

--- map-0x05/main.cpp
+++ map-0x05/main.cpp
@@ -30,6 +30,11 @@ struct Point
 {
     int x;
     int y;
+
+    bool operator==(const Point& other) const
+    {
+        return x == other.x && y == other.y;
+    }
 };
 
 // operator allowing for sorting Points based on the distance from point [0,0]
@@ -38,6 +43,50 @@ bool operator<(const Point lhs, const Point rhs)
     auto lhs_distance_sq = pow(lhs.x, 2) + pow(lhs.y, 2);
     auto rhs_distance_sq = pow(rhs.x, 2) + pow(rhs.y, 2);
     return lhs_distance_sq < rhs_distance_sq;
+}
+
+// injected hash specialization version
+template <>
+struct std::hash<Point>
+{
+    std::size_t operator()(const Point& point) const noexcept
+    {
+        auto hash_sum = std::hash<int> {}(point.x + point.y);
+        auto hash_mult = std::hash<int> {}(point.x * point.y);
+        return hash_sum ^ hash_mult;
+    }
+};
+
+// finds a desired city in an unordered_map and prints coordinates
+// for 3a
+void printCity(const std::unordered_map<std::string, Point>& container,
+               const std::string& cityName)
+{
+    auto search_result = container.find(cityName);
+    if (search_result != container.end()) {
+        std::cout << cityName << " "
+                  << "coords: x="
+                  << search_result->second.x
+                  << " y=" << search_result->second.y << std::endl;
+    }
+}
+
+// finds a desired city in an unordered_map and prints coordinates
+// for 3b
+template <typename Umap>
+void printCity(const Umap& container, const std::string& cityName)
+{
+    auto it = std::ranges::find_if(container,
+                                   [&](const auto& entry) {
+                                       return entry.second == cityName;
+                                   });
+
+    if (it != container.end()) {
+        std::cout << cityName << " "
+                  << "coords: x="
+                  << it->first.x
+                  << " y=" << it->first.y << std::endl;
+    }
 }
 
 int main()
@@ -67,31 +116,43 @@ int main()
     }
 
     // 3a. Pobierz i wypisz współrzędne Sydney
-    auto search_result = citiesA.find("Sydney");
-    if (search_result != citiesA.end()) {
-        std::cout << "Sydney coords: x="
-                  << search_result->second.x
-                  << " y=" << search_result->second.y << std::endl;
-    }
+    printCity(citiesA, "Sydney");
 
+    // PART B
     //  1b. Skopiuj te dane do mapy std::unordered_map<Point, std::string>
+
+    // custom hash for unordered_map<Point,std::string>
     auto PointHash = [](const auto& point) {
-        return std::hash<int> {}(point.x + point.y);
+        auto hash_sum = std::hash<int> {}(point.x + point.y);
+        auto hash_mult = std::hash<int> {}(point.x * point.y);
+        return hash_sum & hash_mult;
     };
 
+    // custom equality compare for unordered_map<Point,std::string>
     auto PointEqual = [](const auto& pointA, const auto& pointB) {
         return pointA.x == pointB.x && pointA.y == pointB.y;
     };
 
+    // NOTE: First opiton with custom hash and lambda equality comparison - works
     std::unordered_map<Point,
                        std::string,
                        decltype(PointHash),
                        decltype(PointEqual)>
         citiesB;
 
+    // NOTE: Second option with injected std::hash specialization and Point::operator==()usage
+    std::unordered_map<Point, std::string> citiesC;
+
     std::transform(citiesA.begin(),
                    citiesA.end(),
                    std::inserter(citiesB, citiesB.begin()),
+                   [](const auto& el) -> std::pair<Point, std::string> {
+                       return { el.second, el.first };
+                   });
+
+    std::transform(citiesA.begin(),
+                   citiesA.end(),
+                   std::inserter(citiesC, citiesC.begin()),
                    [](const auto& el) -> std::pair<Point, std::string> {
                        return { el.second, el.first };
                    });
@@ -105,17 +166,16 @@ int main()
         }
     }
 
-    // //  3b. Pobierz i wypisz współrzędne Sydney
-    auto it = std::ranges::find_if(citiesB,
-                                   [&](const auto& entry) {
-                                       return entry.second == "Sydney";
-                                   });
-
-    if (it != citiesB.end()) {
-        std::cout << "Sydney coords: x="
-                  << search_result->second.x
-                  << " y=" << search_result->second.y << std::endl;
+    for (const auto& [point, city] : citiesC) {
+        auto [is_in_range, range] = point_in_range(point);
+        if (is_in_range) {
+            std::cout << city << "-> distance from origin: " << sqrt(range) << std::endl;
+        }
     }
+
+    // //  3b. Pobierz i wypisz współrzędne Sydney
+    printCity(citiesB, "Sydney");
+    printCity(citiesC, "Sydney");
 
     return 0;
 }
